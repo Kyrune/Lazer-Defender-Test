bd4731ef1d6357919fda689ddfbd0e059f26a63d
Kyrune <jn-01@centurytel.net>
2021-11-17 11:50:10 -0600

    Create file

---
+++ UIGameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Awake() 
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    void Start()
+    {
+        scoreText.text = "you scored:\n" + scoreKeeper.GetScore();
+    }
+
+}
